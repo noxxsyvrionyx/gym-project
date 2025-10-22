94cf12080c57128bdc8dd91e394350a202332b22
noxxsyvrionyx <noxxsyvrionyx@gmail.com>
2025-10-22 23:27:26 +0600

    Configured Vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/gym-project/', // 👈 This line fixes the GitHub Pages blank screen
 })
